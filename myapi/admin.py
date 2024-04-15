cd0f90cfba6c6b44149a5faeb22b87ca4491afff
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-04-15 18:59:08 +0330

    change admin.py change some datetimes to date type and add some data

--- myapi/admin.py
+++ myapi/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(models.GenreMovie)
+admin.site.register(models.GenreMusic)
+admin.site.register(models.Artist)
+admin.site.register(models.Movie)
+admin.site.register(models.Serial)
+admin.site.register(models.Music)
+admin.site.register(models.Image)
