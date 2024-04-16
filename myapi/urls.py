5b078ad24aedb26544ac2b986c6d6e8741522ef0
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-04-16 19:37:15 +0330

    give some names to urls

--- myapi/urls.py
+++ myapi/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('movies/', views.movie_list),
-    path('movies/<int:pk>/', views.movie_detail),
-    path('series/', views.serial_list),
-    path('series/<int:pk>/', views.serial_detail),
-    path('music/', views.music_list),
-    path('music/<int:pk>/', views.music_detail),
+    path('movies/', views.movie_list, name='movies'),
+    path('movies/<int:pk>/', views.movie_detail, name='movie-detail'),
+    path('series/', views.serial_list, name='series'),
+    path('series/<int:pk>/', views.serial_detail, name='serial-detail'),
+    path('music/', views.music_list, name='musics'),
+    path('music/<int:pk>/', views.music_detail, name='music-detail'),
 ]
