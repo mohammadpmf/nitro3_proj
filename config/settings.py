108e5c8dd02543649b968fb76ce096be2ed3a83d
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-04-16 18:12:43 +0330

    use decimal numbers instead of strings by changing settings.py

--- config/settings.py
+++ config/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+
+REST_FRAMEWORK = {
+    'COERCE_DECIMAL_TO_STRING': False,
+}
