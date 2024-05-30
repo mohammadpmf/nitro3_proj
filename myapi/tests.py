f2e18a73dfc3db6e746c1cfdcc8ff03d49091048
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-05-30 16:23:02 +0330

    fix error

--- myapi/tests.py
+++ myapi/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse
+from .models import Movie, Artist
 
 class MyAPITest(TestCase):
     def test_get_first_page_by_url(self):
@@ -19,6 +20,17 @@ class MyAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_movies_detail_by_name(self):
-        response = self.client.get(reverse('movie-detail', args=[1])) # ارور میده. چون یه دیتابیس تست تازه میسازه و توش هیچ فیلمی نیست. پس ۴۰۴ میده. پس باید یه نمونه بسازیم
+        artist1 = Artist.objects.create(first_name="David", last_name="Newman", nick_name="DaNewman")
+        Movie.objects.create(
+            title="A new Movie",
+            year=2024,
+            runtime=112,
+            status="released",
+            director=artist1,
+            country="USA",
+            plot="A man goes to a park and story begins...",
+            rating=7.3
+        )
+        response = self.client.get(reverse('movie-detail', args=[1])) # این طوری دیگه ارور نمیده و پاس میشه. چون تو دیتابیس تست تازه یه آرتیس ساختیم و یه فیلم که کارگردانش همون هست و جزییات فیلم ۱ رو میتونه به ما بده. اما اگه بزنیم فیلم ۲۰ بهمون ارور ۴۰۴ میده چون وجود نداره
         self.assertEqual(response.status_code, 200)
         
