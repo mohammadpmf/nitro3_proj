3d3af200dd2244dc6b37a962763f3943e24a72f7
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-05-30 16:31:40 +0330

    use setUpTestData to avoid code repeat

--- myapi/tests.py
+++ myapi/tests.py
@@ -3,6 +3,21 @@ from django.urls import reverse
 from .models import Movie, Artist
 
 class MyAPITest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.artist1 = Artist.objects.create(first_name="David", last_name="Newman", nick_name="DaNewman")
+        cls.movie = Movie.objects.create(
+            title="A new Movie",
+            year=2024,
+            runtime=112,
+            status="released",
+            director=cls.artist1,
+            country="USA",
+            plot="A man goes to a park and story begins...",
+            rating=7.3
+        )
+
+
     def test_get_first_page_by_url(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -20,17 +35,6 @@ class MyAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_movies_detail_by_name(self):
-        artist1 = Artist.objects.create(first_name="David", last_name="Newman", nick_name="DaNewman")
-        Movie.objects.create(
-            title="A new Movie",
-            year=2024,
-            runtime=112,
-            status="released",
-            director=artist1,
-            country="USA",
-            plot="A man goes to a park and story begins...",
-            rating=7.3
-        )
         response = self.client.get(reverse('movie-detail', args=[1])) # این طوری دیگه ارور نمیده و پاس میشه. چون تو دیتابیس تست تازه یه آرتیس ساختیم و یه فیلم که کارگردانش همون هست و جزییات فیلم ۱ رو میتونه به ما بده. اما اگه بزنیم فیلم ۲۰ بهمون ارور ۴۰۴ میده چون وجود نداره
         self.assertEqual(response.status_code, 200)
         
