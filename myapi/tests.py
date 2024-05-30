703a8070862638946137dd4f2ae92a9096591b42
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-05-30 14:53:42 +0330

    Start testing

--- myapi/tests.py
+++ myapi/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class MyAPITest(TestCase):
+    def test_get_first_page_by_url(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_get_movies_by_url(self):
+        response = self.client.get('/movies/')
+        self.assertEqual(response.status_code, 200)
