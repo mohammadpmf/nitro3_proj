1f42868a559585f9b92c10693a5318be6200f3d6
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-09 21:29:20 +0330

    Add more test

--- myapi/tests.py
+++ myapi/tests.py
@@ -5,7 +5,9 @@ from .models import Movie, Artist
 class MyAPITest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.artist1 = Artist.objects.create(first_name="David", last_name="Newman", nick_name="DaNewman")
+        cls.artist1 = Artist.objects.create(first_name="Jon", last_name="Favreau", birth_date="1966-10-19")
+        cls.artist2 = Artist.objects.create(first_name="Robert", last_name="Downey", nick_name="Robert Downey Jr.", birth_date="1965-04-04")
+        cls.artist3 = Artist.objects.create(first_name="Gwyneth", last_name="Paltrow")
         cls.movie = Movie.objects.create(
             title="A new Movie",
             year=2024,
@@ -14,9 +16,9 @@ class MyAPITest(TestCase):
             director=cls.artist1,
             country="USA",
             plot="A man goes to a park and story begins...",
-            rating=7.3
+            rating=7.3,
         )
-
+        cls.movie.cast.set([cls.artist1, cls.artist2, cls.artist3]) # این رو اجازه نمیده موقع تعریف فیلم بذاریم. به خاطر منی تو منی بودن. خودش میگه که باید از تابع ست استفاده کنیم به جاش.
 
     def test_get_first_page_by_url(self):
         response = self.client.get('/')
@@ -35,7 +37,7 @@ class MyAPITest(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_get_movies_detail_by_name(self):
-        response = self.client.get(reverse('movie-detail', args=[1])) # این طوری دیگه ارور نمیده و پاس میشه. چون تو دیتابیس تست تازه یه آرتیس ساختیم و یه فیلم که کارگردانش همون هست و جزییات فیلم ۱ رو میتونه به ما بده. اما اگه بزنیم فیلم ۲۰ بهمون ارور ۴۰۴ میده چون وجود نداره
+        response = self.client.get(reverse('movie-detail', args=[1])) # این طوری دیگه ارور نمیده و پاس میشه. چون تو دیتابیس تست تازه یه آرتیست ساختیم و یه فیلم که کارگردانش همون هست و جزییات فیلم ۱ رو میتونه به ما بده. اما اگه بزنیم فیلم ۲۰ بهمون ارور ۴۰۴ میده چون وجود نداره
         self.assertEqual(response.status_code, 200)
         
     def test_get_first_name_of_director(self):
@@ -46,3 +48,15 @@ class MyAPITest(TestCase):
         self.assertEqual(name, self.movie.director.first_name) # OK
         # self.assertEqual(name, "Ali") # Fail
         
+    def test_get_nick_name_of_second_cast(self):
+        response = self.client.get(reverse('movie-detail', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        name = data.get('cast')[1].get('nick_name')
+        self.assertEqual(name, self.artist2.nick_name) # OK
+        # self.assertEqual(name, self.artist1.nick_name) # Fail
+
+    def test_get_404_if_page_not_exists(self):
+        response = self.client.get(reverse('movie-detail', args=[1000]))
+        self.assertEqual(response.status_code, 404)
+
