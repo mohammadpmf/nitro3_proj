60e10d1d1d806521d3587bdb1a7212b7130e9c3b
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-06-11 14:06:00 +0330

    Add sms to extra part

--- myapi/urls.py
+++ myapi/urls.py
@@ -4,6 +4,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 from . import views
+from . import views_extra_part
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -25,8 +26,8 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('get_sms/', views_extra_part.get_sms, name='get_sms')
 ]
-
 
 # urlpatterns = [
 #     path('movies/', views.MovieList.as_view(), name='movies'),
---
+++ myapi/views_extra_part.py
@@ -0,0 +1,16 @@
+from django.shortcuts import HttpResponse
+import ghasedakpack
+
+
+def get_sms(request):
+    sms = ghasedakpack.Ghasedak("f889f4f2754a67771b17e6bed88477d582b594f844ff6b4c7b62db2679d98f5d")
+    my_number = '09xxxxxxxxx'
+    answer = sms.send({
+        'message': f'اس ام اس به شماره {my_number} ارسال شد',
+        'receptor' : my_number,
+        'linenumber': '30005006009956'
+        })
+    if answer==True:
+        return HttpResponse(f"اس ام اس با موفقیت به شماره {my_number} ارسال شد")
+    else:
+        return HttpResponse("در ارسال اس ام اس مشکلی پیش آمد")
