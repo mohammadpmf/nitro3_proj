2f78f9a09c35f740a2a64690dc9f595fb126156d
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-15 12:02:00 +0330

    user reformatting to make urls more readable

--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapi.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("myapi.urls")),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.jwt")),
     path("__debug__/", include("debug_toolbar.urls")),
--- myapi/urls.py
+++ myapi/urls.py
@@ -11,29 +11,35 @@ schema_view = get_schema_view(
         title="Movies API",
         default_version="v1",
     ),
-    public=True, # defalut e public=False hast. age kar nakard in khat ro vardaram
+    public=True,  # defalut e public=False hast. age kar nakard in khat ro vardaram
 )
 
 router = DefaultRouter()
-router.register('movies', views.MovieViewSet, basename='movie')
-router.register('series', views.SerialViewSet, basename='serial')
-router.register('music', views.MusicViewSet, basename='music')
-router.register('staff', views.StaffViewSet, basename='staff')
+router.register("movies", views.MovieViewSet, basename="movie")
+router.register("series", views.SerialViewSet, basename="serial")
+router.register("music", views.MusicViewSet, basename="music")
+router.register("staff", views.StaffViewSet, basename="staff")
 
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('get_sms/', views_extra_part.get_sms, name='get_sms')
+    path("", include(router.urls)),
+    path(
+        "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("get_sms/", views_extra_part.get_sms, name="get_sms"),
 ]
 
 # urlpatterns = [
-#     path('movies/', views.MovieList.as_view(), name='movies'),
-#     path('movies/<int:pk>/', views.MovieDetail.as_view(), name='movie-detail'),
-#     path('series/', views.SerialList.as_view(), name='series'),
-#     path('series/<int:pk>/', views.SerialDetail.as_view(), name='serial-detail'),
-#     path('music/', views.MusicList.as_view(), name='musics'),
-#     path('music/<int:pk>/', views.MusicDetail.as_view(), name='music-detail'),
+#     path("movies/", views.MovieList.as_view(), name="movies"),
+#     path("movies/<int:pk>/", views.MovieDetail.as_view(), name="movie-detail"),
+#     path("series/", views.SerialList.as_view(), name="series"),
+#     path("series/<int:pk>/", views.SerialDetail.as_view(), name="serial-detail"),
+#     path("music/", views.MusicList.as_view(), name="musics"),
+#     path("music/<int:pk>/", views.MusicDetail.as_view(), name="music-detail"),
 # ]
