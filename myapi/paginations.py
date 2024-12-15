93f5eb573003f38fe4c72b7b57118baf982cfddb
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-15 12:03:50 +0330

    use reformatting to make paginations more readable

--- myapi/paginations.py
+++ myapi/paginations.py
@@ -2,12 +2,12 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class MoviePagination(PageNumberPagination):
-    page_size=10
+    page_size = 10
 
 
 class SerialPagination(PageNumberPagination):
-    page_size=10
+    page_size = 10
 
 
 class MusicPagination(PageNumberPagination):
-    page_size=20
+    page_size = 20
