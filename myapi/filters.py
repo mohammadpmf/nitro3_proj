d9f03f1e8c14c111f18aebbb76347622898a5534
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-15 12:14:55 +0330

    reformat filters.py to make it more readable

--- myapi/filters.py
+++ myapi/filters.py
@@ -7,22 +7,22 @@ class MovieFilter(FilterSet):
     class Meta:
         model = Movie
         fields = {
-            'title': ['icontains'],
-            'year': ['range', 'exact', 'lte', 'gte'],
-            'status': ['exact'],
-            'director': ['exact'],
-            # 'director__first_name': ['icontains'],
-            # 'director__last_name': ['icontains'],
-            # 'director__nick_name': ['icontains'],
-            'country': ['icontains'],
-            'plot': ['icontains'],
-            'rating': ['range'],
-            'genre': ['exact'],
-            'genre__title': ['icontains'],
-            'cast': ['exact'],
-            # 'cast__first_name': ['icontains'],
-            # 'cast__last_name': ['icontains'],
-            # 'cast__nick_name': ['icontains'],
+            "title": ["icontains"],
+            "year": ["range", "exact", "lte", "gte"],
+            "status": ["exact"],
+            "director": ["exact"],
+            # "director__first_name": ["icontains"],
+            # "director__last_name": ["icontains"],
+            # "director__nick_name": ["icontains"],
+            "country": ["icontains"],
+            "plot": ["icontains"],
+            "rating": ["range"],
+            "genre": ["exact"],
+            "genre__title": ["icontains"],
+            "cast": ["exact"],
+            # "cast__first_name": ["icontains"],
+            # "cast__last_name": ["icontains"],
+            # "cast__nick_name": ["icontains"],
         }
 
 
@@ -30,22 +30,22 @@ class SerialFilter(FilterSet):
     class Meta:
         model = Serial
         fields = {
-            'title': ['icontains'],
-            'begin_year': ['range'],
-            'end_year': ['range'],
-            'director': ['exact'],
-            # 'director__first_name': ['icontains'],
-            # 'director__last_name': ['icontains'],
-            # 'director__nick_name': ['icontains'],
-            'country': ['icontains'],
-            'plot': ['icontains'],
-            'rating': ['range'],
-            'genre': ['exact'],
-            'genre__title': ['icontains'],
-            'cast': ['exact'],
-            # 'cast__first_name': ['icontains'],
-            # 'cast__last_name': ['icontains'],
-            # 'cast__nick_name': ['icontains'],
+            "title": ["icontains"],
+            "begin_year": ["range"],
+            "end_year": ["range"],
+            "director": ["exact"],
+            # "director__first_name": ["icontains"],
+            # "director__last_name": ["icontains"],
+            # "director__nick_name": ["icontains"],
+            "country": ["icontains"],
+            "plot": ["icontains"],
+            "rating": ["range"],
+            "genre": ["exact"],
+            "genre__title": ["icontains"],
+            "cast": ["exact"],
+            # "cast__first_name": ["icontains"],
+            # "cast__last_name": ["icontains"],
+            # "cast__nick_name": ["icontains"],
         }
 
 
@@ -53,10 +53,10 @@ class MusicFilter(FilterSet):
     class Meta:
         model = Music
         fields = {
-            'title': ['icontains'],
-            'year': ['range', 'exact', 'lte', 'gte'],
-            'main_singer': ['exact'],
-            'genre': ['exact'],
-            'genre__title': ['icontains'],
-            'other_singers': ['exact'],
+            "title": ["icontains"],
+            "year": ["range", "exact", "lte", "gte"],
+            "main_singer": ["exact"],
+            "genre": ["exact"],
+            "genre__title": ["icontains"],
+            "other_singers": ["exact"],
         }
