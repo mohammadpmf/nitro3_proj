f6c95ea86de877df09f81a63373795b00bc3596a
Mohammad Pourmohammadi Fallah <mohammad.pfallah@gmail.com>
2024-12-15 12:03:04 +0330

    user reformatting to make signals more readable

--- myapi/signals.py
+++ myapi/signals.py
@@ -3,8 +3,8 @@ from django.dispatch import receiver
 from django.conf import settings
 from .models import Staff
 
+
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_staff_profile_for_created_staff(sender, instance, created, **kwargs):
     if created:
         Staff.objects.create(user=instance)
-        
